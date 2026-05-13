ce1449735b1eb08f78495fcf7d754bc4541e50eb
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2026-05-13 13:26:04 +0200

    Modifications sur la bdd

--- schema.sql
+++ schema.sql
@@ -16,8 +16,9 @@ CREATE TABLE Detections(
 	id_detection INTEGER AUTO_INCREMENT PRIMARY KEY,
     id_session INTEGER,
     FOREIGN KEY (id_session) REFERENCES Sessions(id_session),
-    emotion ENUM('joie', 'tristesse', 'colere', 'surprise', 'peur'),
+    emotion ENUM('joie', 'tristesse', 'colere', 'peur'),
     heure DATETIME,
-    landmarks JSON
+    landmarks JSON,
+    important BOOLEAN
     
 );
